50f68fcef7353a82d9d89ce99a58f576686e628f
padala kowshik <padalakowshik05@gmail.com>
2026-08-21 15:29:16 +0530

    ✅ PREFPRO2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/PREFPRO2/solution.java
+++ codechef/medium/PREFPRO2/solution.java
@@ -1,26 +1,20 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    Scanner sc = new Scanner(System.in);
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         long[] prefix = new long[n + 1];
 
-        // Build prefix sum
         for (int i = 1; i <= n; i++) {
-            int x = sc.nextInt();
-            prefix[i] = prefix[i - 1] + x;
+            int num = sc.nextInt();
+            prefix[i] = prefix[i - 1] + num;
         }
 
         int k = sc.nextInt();
 
-        // Answer queries
         while (k-- > 0) {
             int a = sc.nextInt();
             int b = sc.nextInt();
@@ -30,5 +24,6 @@ class Codechef
             System.out.println(sum);
         }
 
-	}
+        sc.close();
+    }
 }
