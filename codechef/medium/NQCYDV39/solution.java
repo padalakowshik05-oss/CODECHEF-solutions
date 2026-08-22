5012c43e44e75266ad5652b6db4e552f38399884
padala kowshik <padalakowshik05@gmail.com>
2026-08-22 23:38:39 +0530

    ✅ NQCYDV39 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

---
+++ codechef/medium/NQCYDV39/solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class CodeChef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("" + s.charAt(0) + s.charAt(2) + s.charAt(5));
+    }
+}
