ca91932c0109462cc788582675ba1ebf4e93bb9e
padala kowshik <padalakowshik05@gmail.com>
2026-08-22 23:38:27 +0530

    ✅ NQCYDV38 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-22

--- codechef/medium/NQCYDV38/solution.java
+++ codechef/medium/NQCYDV38/solution.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 class CodeChef {
     public static void main(String[] args) {
-        int num1 = 10, num2 = 20, num3 = 30;
-        double average = (num1 + num2 + num3) / 3;  // Logical error: Wrong precedence
-
-        System.out.println("The calculated average is: " + average); // it will print 40.0, which is wrong
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("" + s.charAt(0) + s.charAt(2) + s.charAt(5));
     }
 }
