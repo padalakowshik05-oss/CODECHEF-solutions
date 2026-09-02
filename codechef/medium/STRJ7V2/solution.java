83df08e1c4dd847de13ee99abfb5c3f43a9ef42f
padala kowshik <padalakowshik05@gmail.com>
2026-09-02 23:16:02 +0530

    ✅ STRJ7V2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/STRJ7V2/solution.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	   // Update the blank in code given below
+		String var = "String";
+        System.out.print(var.charAt(0));  // Print the first 3 characters
+        System.out.print(var.charAt(1));
+        System.out.print(var.charAt(2));
+	}
+}
