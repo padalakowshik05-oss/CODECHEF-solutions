d9e06750faa28156f1bf1b260356aa61003273df
padala kowshik <padalakowshik05@gmail.com>
2026-09-06 22:07:22 +0530

    ✅ LJAAS40 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/LJAAS40/solution.java
@@ -0,0 +1,15 @@
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        String one = "Coding";
+        String two = "on";
+        String three = "CodeChef";
+        String four = one + " " + two + " " + three;
+
+        System.out.println(one + " - " + one.length());
+        System.out.println(two + " - " + two.length());
+        System.out.println(three + " - " + three.length());
+        System.out.println(four + " - " + four.length());
+    }
+}
