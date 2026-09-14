9c318d3e08bccf594bdc7e2be284d97a199069ec
padala kowshik <padalakowshik05@gmail.com>
2026-09-14 19:36:19 +0530

    ✅ MATMCQ1 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/medium/MATMCQ1/solution.cpp
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n*n;i++){
+            System.out.println(i);
+        }
+
+	}
+}
