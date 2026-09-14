e117e7079ea6b163e9332e540280d91bbe3d3e32
padala kowshik <padalakowshik05@gmail.com>
2026-09-14 19:59:30 +0530

    ✅ MATMULTIPLIC | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/MATMULTIPLIC/solution.java
+++ codechef/medium/MATMULTIPLIC/solution.java
@@ -1,25 +1,48 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int m=sc.nextInt();
-		int[][] a=new int[n][m];
-		int count=0;
-		for(int i=0;i<n;i++){
-		    for(int j=0;j<m;j++){
-		        a[i][j]=sc.nextInt();
-		        if(a[i][j]<0){
-		            count++;
-		        }
-		    }
-		}
-		System.out.println(count);
+class Codechef {
+    public static void main(String[] args) {
 
-	}
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] a = new int[m][n];
+
+        // Matrix A
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        int n2 = sc.nextInt();
+        int p = sc.nextInt();
+
+        int[][] b = new int[n2][p];
+
+        // Matrix B
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < p; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Matrix multiplication
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+
+                int sum = 0;
+
+                for (int k = 0; k < n; k++) {
+                    sum += a[i][k] * b[k][j];
+                }
+
+                System.out.print(sum + " ");
+            }
+
+            System.out.println();
+        }
+    }
 }
