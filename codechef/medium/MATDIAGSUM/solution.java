3a8881b1b7ec439b1ef25d322e13d4ff3902e5ae
padala kowshik <padalakowshik05@gmail.com>
2026-09-14 19:54:17 +0530

    ✅ MATDIAGSUM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/MATDIAGSUM/solution.java
+++ codechef/medium/MATDIAGSUM/solution.java
@@ -6,20 +6,26 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int m=sc.nextInt();
-		int[][] a=new int[n][m];
-		for(int i=0;i<n;i++){
-		    for(int j=0;j<m;j++){
-		        a[i][j]=sc.nextInt();
-		    }
-		}
-		for(int i=n-1;i>=0;i--){
-		    for(int j=0;j<m;j++){
-		        System.out.print(a[i][j]+" ");
-		    }
-		    System.out.println();
-		}
+	    Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] a = new int[n][n];
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                a[i][j] = sc.nextInt();
+
+                if (i == j || i + j == n - 1) {
+                    sum += a[i][j];
+                }
+            }
+        }
+
+        System.out.println(sum);
+
 	}
 }
