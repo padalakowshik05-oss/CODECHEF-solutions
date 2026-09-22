ee630ae2052534eb1b1ec8ce57e3b0193e5e908a
padala kowshik <padalakowshik05@gmail.com>
2026-09-22 23:14:59 +0530

    ✅ Count all triplets with given sum in sorted array | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-22

--- gfg/medium/count-all-triplets-with-given-sum-in-sorted-array/solution.java
+++ gfg/medium/count-all-triplets-with-given-sum-in-sorted-array/solution.java
@@ -4,47 +4,41 @@ class Solution {
         Arrays.sort(arr);
 
         int count = 0;
-        int n = arr.length;
 
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < arr.length - 2; i++) {
 
             int left = i + 1;
-            int right = n - 1;
+            int right = arr.length - 1;
 
             while (left < right) {
 
-                long sum = (long) arr[i] + arr[left] + arr[right];
+                int sum = arr[i] + arr[left] + arr[right];
 
                 if (sum == target) {
 
                     if (arr[left] == arr[right]) {
-
-                        int size = right - left + 1;
-                        count += (size * (size - 1)) / 2;
+                        int n = right - left + 1;
+                        count += n * (n - 1) / 2;
                         break;
-
-                    } else {
-
-                        int leftValue = arr[left];
-                        int rightValue = arr[right];
-
-                        int leftCount = 0;
-                        int rightCount = 0;
-
-                        while (left <= right &&
-                               arr[left] == leftValue) {
-                            leftCount++;
-                            left++;
-                        }
-
-                        while (right >= left &&
-                               arr[right] == rightValue) {
-                            rightCount++;
-                            right--;
-                        }
-
-                        count += leftCount * rightCount;
                     }
+
+                    int leftValue = arr[left];
+                    int rightValue = arr[right];
+
+                    int leftCount = 0;
+                    int rightCount = 0;
+
+                    while (left <= right && arr[left] == leftValue) {
+                        left++;
+                        leftCount++;
+                    }
+
+                    while (right >= left && arr[right] == rightValue) {
+                        right--;
+                        rightCount++;
+                    }
+
+                    count += leftCount * rightCount;
 
                 } else if (sum < target) {
                     left++;
