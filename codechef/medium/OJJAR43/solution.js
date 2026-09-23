c0c18eafaa171ae953f56902180e7449e23ba060
padala kowshik <padalakowshik05@gmail.com>
2026-09-23 11:40:15 +0530

    ✅ OJJAR43 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/OJJAR43/solution.js
@@ -0,0 +1,7 @@
+let words = ["apple", "banana", "pear", "kiwi", "grape", "orange"];
+let shortWords=words.filter((word) => {
+    return word.length<=4;
+});
+console.log(shortWords);
+
+// complete the code 
