7488185d51124da434afa67cd2e69f4a159910da
padala kowshik <padalakowshik05@gmail.com>
2026-09-23 11:29:41 +0530

    ✅ OJJAR36 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/OJJAR36/solution.js
@@ -0,0 +1,10 @@
+const products = [
+  { name: 'Laptop', price: 1200 },
+  { name: 'Mouse', price: 25 },
+  { name: 'Keyboard', price: 75 }
+];
+
+ 
+const prices=products.map(({price}) => price);
+
+console.log(prices);
