acc502078a389aab33ec1a2cf986d7fd62e77e7d
padala kowshik <padalakowshik05@gmail.com>
2026-09-24 21:46:00 +0530

    ✅ DSAPROB15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ codechef/medium/DSAPROB15/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static long countPairsLessThanX(int[] arr, int x) {
+        long count = 0;
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            if (arr[i] + arr[j] < x) {
+                count += j - i;
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        System.out.println(countPairsLessThanX(arr, x));
+    }
+}
