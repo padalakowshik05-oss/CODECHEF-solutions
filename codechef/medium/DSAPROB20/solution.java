04962742a742bfb5fb16af4dbd124ea201097b2b
padala kowshik <padalakowshik05@gmail.com>
2026-09-24 21:26:58 +0530

    ✅ DSAPROB20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

--- codechef/medium/DSAPROB20/solution.java
+++ codechef/medium/DSAPROB20/solution.java
@@ -9,15 +9,16 @@ public class Main {
         int j=0;
         int largest=0;
         while(i<arr1.length && j<arr2.length){
-            if(arr1[i]<arr2[j]){
-                i++;
-            }
-            else if(arr1[i]>arr2[j]){
-                j++;
-            }
-            else{
+            if(arr1[i]==arr2[j]){
                 largest=arr1[i];
                 i++;
+                j++;
+            }
+            else if(arr1[i]<arr2[j]){
+                i++;
+            }
+            else{
+                
                 j++;
             }
         }
