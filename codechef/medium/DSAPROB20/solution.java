aee3dc3eb2663eccabdfb673db6fd3a993793b20
padala kowshik <padalakowshik05@gmail.com>
2026-09-24 21:27:16 +0530

    ✅ DSAPROB20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

--- codechef/medium/DSAPROB20/solution.java
+++ codechef/medium/DSAPROB20/solution.java
@@ -7,7 +7,7 @@ public class Main {
         Arrays.sort(arr2);
         int i=0;
         int j=0;
-        int largest=0;
+        int largest=-1;
         while(i<arr1.length && j<arr2.length){
             if(arr1[i]==arr2[j]){
                 largest=arr1[i];
